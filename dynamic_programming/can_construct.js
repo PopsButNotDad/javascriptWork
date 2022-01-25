b341b8db80f87253a212e1c86ba280d7949dbeb5
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-01-25 14:49:54 -0800

    Create can_construct.js

---
+++ dynamic_programming/can_construct.js
@@ -0,0 +1,9 @@
+//Write a function `canConstruct(target, wordbank)` that accepts a target string and an array of strings.
+
+//The function should return a boolean indicating whether or not the `target` can be constructed by concatenating
+    //elemends of the `wordbank` array.
+
+//You may reuse the elements of the `wordbank` as many times as needed
+
+
+ 
