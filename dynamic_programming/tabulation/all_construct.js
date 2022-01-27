7555aa62732a2cd7c0dc855309a75eef5918bd6f
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-01-27 14:39:39 -0800

    Update all_construct.js

--- dynamic_programming/tabulation/all_construct.js
+++ dynamic_programming/tabulation/all_construct.js
@@ -6,3 +6,58 @@
 
 //You may reuse elements of `wordBank` as many times as needed.
 
+
+const allConstruct = (target, wordbank) => {
+    //.map makes it so the table is filled with individual arrays and not one array across the whole table
+    const table = Array(target.length + 1)
+    .fill()
+    .map(() => []);
+
+    table[0] = [[]];
+
+    for(let i = 0; i <= target.length; i++){
+        for(let word of wordbank){
+            if(target.slice(i, i + word.length) === word){
+                const newCombinations = table[i].map(subArray => [...subArray, word]);
+                table[i + word.length].push(...newCombinations);
+            }
+        }
+    };
+
+    return table[target.length];
+
+};
+
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl'])); 
+//[
+    //[ 'purp' , 'le' ]
+    //[ 'p', 'ur', 'p', 'le' ]
+//]
+
+console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c'])); 
+//[
+    //[ 'ab', 'cd', 'ed' ]  
+    //[ 'ab', 'c', 'def' ]
+    //[ 'abc', 'def' ]
+    //[ 'abcd', 'ef' ]
+//]
+
+console.log(allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); 
+//[]
+
+console.log(allConstruct('aaaaaaaaaaz', [
+    'a',
+    'aa',
+    'aaa',
+    'aaaa',
+    'aaaaa',
+])); // [] 
+
+
+//Complexity
+    //m = target.length
+    //n = wordbank.length
+
+    //Time: O( n^m )
+    //Space: O( n^m )
+
