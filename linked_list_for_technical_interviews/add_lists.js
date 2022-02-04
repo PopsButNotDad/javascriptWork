f25f6714d2854c18935973316f8b1cc392e3b4b9
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-04 14:46:11 -0800

    Update add_lists.js

--- linked_list_for_technical_interviews/add_lists.js
+++ linked_list_for_technical_interviews/add_lists.js
@@ -11,27 +11,42 @@ class Node {
 }
   
 const addLists = (head1, head2) => {
+    const dummyHead = new Node(null);
+    let tail = dummyhead;
+    let carry = 0;
     let current1 = head1;
     let current2 = head2;
 
-    while(current1 !== null || current2 !== null){
+    while(current1 !== null || current2 !== null || carry === 1){
+        const val1 = current1 === null ? 0 : current1.val;
+        const val2 = current2 === null ? 0 : current2.val;
 
+        const sum = val1 + val2 + carry;
+        carry = sum > 9 ? 1 : 0;
+        const digit = sum % 10;
 
+        if(current1 !== null)current1 = current1.next;
+            
+        if(current2 !== null)current2 = current2.next;
+        
+
+        tail.next = new Node(digit);
+        tail = tail.next;
 
     }
-
-
+    
+    return dummyHead.next;
 
 };
 
 const addListsRe = (head1, head2, carry = 0) => {
-    if(head1 === null && head2 === null) return null;
+    if(head1 === null && head2 === null && carry === 0) return null;
     const val1 = head1 === null ? 0 : head1.val;
     const val2 = head2 === null ? 0 : head2.val;
     const sum = val1 + val2 + carry;
     const nextCarry = sum > 9 ? 1 : 0;
     const digit = sum % 10;
-    const resultNode = new Node(sum);
+    const resultNode = new Node(digit);
 
 
     const next1 = head1 === null ? null : head1.next;
