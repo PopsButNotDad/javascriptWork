df9d039248bee00b601c9673ad7136c1c59c11a6
PopsButNotDad <hogan.coverdale@wsu.edu>
2022-02-03 20:39:00 -0800

    Update insert.node.js

--- linked_list_for_technical_interviews/insert.node.js
+++ linked_list_for_technical_interviews/insert.node.js
@@ -14,8 +14,32 @@ class Node {
 }
   
 const insertNode = (head, value, index) => {
+    if(index === 0){
+        const newHead = new Node(value);
+        newHead.next = head;
+        return newHead;
+    }
+  
+    let current = head;
+    let count = 0;
 
+    while(current !== null) {
+        if(count === index - 1){
+            const temp = current.next;
+            current.next = new Node(value);
+            current.next.next = temp;
+        }
+        count++;
+        current = current.next;
+    }
+
+    return head;
+    
 };
+
+const insertNodeRe = (head, value, index) => {
+    
+}
 
 const a = new Node("a");
 const b = new Node("b");
