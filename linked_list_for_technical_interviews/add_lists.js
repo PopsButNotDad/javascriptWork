c69b8beab3d883d635d4084584b427244d75d223
PopsButNotDad <77769004+PopsButNotDad@users.noreply.github.com>
2022-02-04 14:22:51 -0800

    Update add_lists.js

--- linked_list_for_technical_interviews/add_lists.js
+++ linked_list_for_technical_interviews/add_lists.js
@@ -5,14 +5,32 @@
 
 class Node {
     constructor(val) {
-      this.val = val;
-      this.next = null;
+        this.val = val;
+        this.next = null;
     }
-  }
+}
   
-  const addLists = (head1, head2) => {
-    // todo
-  };
+const addLists = (head1, head2) => {
+    let current1 = head1;
+    let current2 = head2;
+
+    while(current1 !== null && current2 !== null){
+
+
+
+    }
+
+
+    if(current1 === null){
+
+    }
+
+    if(current2 === null){
+
+    }
+
+
+};
 
 //   621
 // + 354
